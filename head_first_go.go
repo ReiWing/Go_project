3d4ca987464e8cf17ce69cf7e48d6a9d868baad4
ReiWing <antonmoiseev6400@gmail.com>
2018-11-06 23:37:39 +0300

    06.11_1

--- head_first_go.go
+++ head_first_go.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"math"
 	"strings"
 )
 
 func main() {
-	math.Floor(2.75)
-	strings.Title("head first go")
-
+	fmt.Println(math.Floor(2.75))
+	fmt.Println(strings.Title("head first go"))
+	fmt.Println('Ж')
 }
