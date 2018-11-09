0757720aef670b4c527ffa70d2240e5e2d2b1719
ReiWing <antonmoiseev6400@gmail.com>
2018-11-10 01:54:24 +0300

    70120

--- pass_fail.go
+++ pass_fail.go
@@ -1,15 +1,26 @@
-// pass_fail reports whether a grade is passing or failing
+// pass_fail reports whether a grade is passing or failings
 package main
 
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 )
 
 func main() {
 	fmt.Print("Enter a grade: ")
 	reader := bufio.NewReader(os.Stdin)
-	input := reader.ReadString('\n')
-	fmt.Println(reader)
+	input, error := reader.ReadString('\n')
+	if score == 100 {
+		fmt.Println("Flawless victory!")
+	} else score >=60 {
+		fmt.Println("Go ahead!")
+	} else {
+		fmt.Println("You shell not pass!")
+	}
+
+	}
+	log.Fatal(error)
+	fmt.Println(input)
 }
