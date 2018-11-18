514c63204b5aade6b59e4a169ba0e83ebdd4d492
ReiWing <antonmoiseev6400@gmail.com>
2018-11-18 13:40:31 +0300

    98 page

--- headFirst/paintNeeded.go
+++ headFirst/paintNeeded.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func paintNeeded(width float64, heigth float64) float64 {
 	area := width * heigth
@@ -8,6 +10,12 @@ func paintNeeded(width float64, heigth float64) float64 {
 }
 
 func main() {
+
+	// err := errors.New("the height should be positive")
+	// fmt.Println(err.Error())
+	// log.Fatal(err)
+	// fmt.Printf("a height of %0.2f is invalid", -2.3333)
+
 	var amount, total float64
 	amount = paintNeeded(4.2, 5.6)
 	fmt.Printf("%0.2f liters needed.\n", amount)
